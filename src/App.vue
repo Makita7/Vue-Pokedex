35f1e8931660b2496f668e14679bb5d55e41588c
sofia-makita <smakita@iadb.org>
2023-05-16 14:40:49 -0300

    adding card style

--- src/App.vue
+++ src/App.vue
@@ -8,19 +8,30 @@
         return{
           hi: 'hi',
           pokemon: [],
+          imgUrl: '',
+          gifUrl: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/versions/generation-v/black-white/animated/',
         }
       },
       methods: {
-        // async fetchPokemon(){
-        //   const res = await axios.get('https://pokeapi.co/api/v2/pokemon/?limit=10&offset=10');
-        //   this.pokemon = res.data;
-        // }
+        async fetchPokemon(){
+          const res = await axios
+            .get('https://pokeapi.co/api/v2/pokemon/?limit=10&offset=10')
+            .then((res) => {
+              sessionStorage.setItem("pokemon", JSON.stringify(res.data));
+              return this.pokemon = res.data;
+            })
+            .catch(error => {
+              console.log(error);
+            });
+        },
       },
-      // async mounted() {
-      //   if( this.pokemon.length !== 0 ){
-      //     await this.fetchPokemon();
-      //   }
-      // },
+      async mounted() {
+        if( sessionStorage.getItem("pokemon") ){
+          this.pokemon = JSON.parse(sessionStorage.getItem("pokemon"))
+        } else{
+          await this.fetchPokemon();
+        }
+      },
       components:{
         AppNavbar,
       },
@@ -31,16 +42,32 @@
 <template>
   <v-app>
     <AppNavbar/>
-    <v-card>
-      <p v-for="i in pokemon.results" :key="i.url">{{ i.name }}</p>
-    </v-card>
+    <div class="d-flex mt-16 pt-4 topFix flex-wrap justify-space-evenly">
+      <v-card class="pokeCard pa-2 ma-4" v-for="i in pokemon.results" :key="i.url">
+        <p class="text-center text-capitalize mt-3">{{ i.name }}</p>
+        <img class="ma-3" :src='gifUrl + i.url.slice(34, 36) + ".gif"' />
+        <v-card-actions class="text-right">
+          <v-spacer/>
+          <p class="text-right font-weight-medium mt-auto mb-0">{{ i.url.slice(34, 36) }}</p>
+        </v-card-actions>
+      </v-card>
+    </div>
     <v-card class="mt-8">
       <RouterView/>
-
     </v-card>
   </v-app>
 </template>
 
-<style scoped>
-
+<style lang="scss" scoped>
+  .topFix{
+    top: 64px;
+  }
+  .pokeCard{
+    width: 16rem;
+    height: 14rem;
+    text-align: center;
+    img{
+      min-width: 3rem;
+    }
+  }
 </style>
